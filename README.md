9aee8ce5e425960c1c99351aeefc42e3a1d9d0e5
oleglego94 <olegik94b@gmail.com>
2020-07-23 00:25:27 +0300

    create Makefile, create cli.py, use cli.py in brain_games.py, modified README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is my first project on Hexlet.
+I'm using Semantic Versioning 2.0.0.
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,8 @@
+from brain_games.cli import welcome_user
+
 def main():
-    print("Welcome to the Brain Games!")
+    print("Welcome to the Brain Games!\n")
+    welcome_user()
 
 if __name__ == '__main__':
     main()
