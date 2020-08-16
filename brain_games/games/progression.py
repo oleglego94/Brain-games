52e783aa64c8d7d53f636146f7ac63c7dfba78c0
oleglego94 <olegik94b@gmail.com>
2020-08-16 22:04:09 +0300

    fix linter errors

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -8,20 +8,21 @@ START_MSG = 'What number is missing in the progression?'
 
 
 def make_set(start, step, miss):
-    set_of_numbers = f''
+    set_of_numbers = ''
     i = 1
     set_length = 10
     while i < miss:
-        elem = start + step * i 
+        elem = start + step * i
         set_of_numbers += f'{elem} '
         i += 1
     set_of_numbers += '.. '
     i += 1
     while i <= set_length:
-        elem = start + step * i 
+        elem = start + step * i
         set_of_numbers += f'{elem} '
         i += 1
     return set_of_numbers
+
 
 def generate_qa():
     start = random.randint(-100, 100)
